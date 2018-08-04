773eae6a40f1cb128d9582f9850c532578dfdb6e
maurice@alu <jmulisa15@alustudent.com>
2018-08-04 16:50:55 +0400

    fixed select2

--- conf.php
+++ conf.php
@@ -2,7 +2,7 @@
 session_start();
 //connect to db
 $dbhost = "localhost"; // this will ususally be 'localhost', but can sometimes differ
-$dbname = "essa_insead"; // the name of the database that you are going to use for this project
+$dbname = "essa_system_db"; // the name of the database that you are going to use for this project
 $dbuser = "root"; // the username that you created, or were given, to access your database
 $dbpass = ""; // the password that you created, or were given, to access your database
  
